e097841fdc19715882fc1e0a99d14cc2e527303f
Yas <irfanyasiras@gmail.com>
2022-01-03 07:41:55 +0700

    update flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,10 +30,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  static const _channel = MethodChannel('/sdk');
+  var _platform = "?";
 
-  void _incrementCounter() {
+  void _incrementCounter() async {
+    var platform = await _channel.invokeMethod('platformName');
     setState(() {
       _counter++;
+      _platform = platform;
     });
   }
 
@@ -46,9 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            Text('You have pushed the button this many times on $_platform'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
