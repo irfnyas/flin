42068fd8b5455abb11e922c1c63195d38b4106fa
Yas <irfanyasiras@gmail.com>
2022-01-03 07:30:36 +0700

    update android

--- android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
@@ -1,6 +1,28 @@
 package com.example.myapp
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+import com.example.myapp.kmm.processMethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
+    override fun configureFlutterEngine(
+      flutterEngine: FlutterEngine
+    ) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        MethodChannel(
+          flutterEngine.dartExecutor.binaryMessenger, "/sdk"
+        ).apply {
+            setMethodCallHandler { call, result ->
+                processMethodChannel(
+                    call.method ?: "",
+                    call.arguments ?: emptyList<String>(),
+                    { s -> result.success(s) },
+                    { e, e1, e2 -> result.error(e, e1, e2) }
+                )
+            }
+        }
+    }
 }
